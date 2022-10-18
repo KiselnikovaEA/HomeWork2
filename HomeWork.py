9fb9ba7e7a85b8ccd6dc943b8f4d41beefb235ee
KisEA <maslennikvakat@mail.ru>
2022-10-18 19:41:47 +0300

    Initial commit - Добавление решения четырёх задач ДЗ

---
+++ HomeWork.py
@@ -0,0 +1,54 @@
+# Task 1
+
+# summ_of_digits = 0
+# some_num_str = input('Введите число N: ')
+# some_num_str = some_num_str.replace('.', '')
+# some_array = [int(i) for i in some_num_str]
+# print(some_array)
+
+# summ = 0
+# for i in some_array:
+#     summ = summ + i
+# print(summ)
+
+#------------------------------------------------------------------------------------
+
+# Task 2
+
+# some_num = int(input('Введите число N: '))
+# some_array = []
+
+# for i in range(1, some_num + 1):
+#     s = 1
+
+#     for j in range(1, i + 1):
+#         s = s * j
+#     print(s)   
+#     some_array.append(s)
+
+# print(some_array)
+
+#-------------------------------------------------------------------------------------
+
+# Task 3
+
+# some_num = int(input('Введите число N: '))
+# some_array = []
+# summ = 0
+# for i in range(1, some_num + 1):
+#     summ = summ + round((1 + 1/i)**i, 2)
+#     some_array.append(round((1 + 1/i)**i, 2))
+
+# print(some_array)
+# print(summ)
+
+#------------------------------------------------------------------------------------
+
+# Task 4 - Перемешивание списка
+
+# import random
+
+# lst = list(range(1,21))
+# print(lst)
+# random.shuffle(lst)
+# print(lst)
